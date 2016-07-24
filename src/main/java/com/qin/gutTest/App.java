2ee1a65dc48e312f9053b85cbb8873f46b0b6d1f
lianfa.qin <lianfa.qin@qunar.com>
2016-07-24 11:24:20 +0800

    lianfa commit

---
+++ src/main/java/com/qin/gutTest/App.java
@@ -0,0 +1,13 @@
+package com.qin.gutTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
